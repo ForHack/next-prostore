61d48db63c71fd4ae810284cd31bf54b9edf9617
Odiljon <olimjonov-odiljon@mail.ru>
2025-02-28 23:34:09 +0500

    first commit

---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,21 @@
+'use server';
+
+import { convertToPlainObject } from '@/lib/utils';
+import { LATEST_PRODUCTS_LIMIT } from '@/lib/constants';
+import { prisma } from '@/db/prisma';
+
+// Get latest products
+export async function getLatestProducts() {
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: { createdAt: 'desc' },
+  });
+
+  return convertToPlainObject(data);
+}
+
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug },
+  });
+}
