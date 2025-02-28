b02fd319a0f26a82f727de87abe235d5437310da
Odiljon <olimjonov-odiljon@mail.ru>
2025-02-28 23:44:59 +0500

    fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
 import ProductList from '@/components/shared/product/product-list';
 import { getLatestProducts } from '@/lib/actions/product.actions';
+import { Product } from '@/types';
 
 const Homepage = async () => {
-  const latestProducts = await getLatestProducts();
-  console.log(latestProducts);
+  const latestProducts = (await getLatestProducts()) as unknown as Product[];
 
   return (
     <>
